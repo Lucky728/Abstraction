ce3a2786b271fc4bb67bb69b59be0bd9f414a329
Lucky728 <61776822+Lucky728@users.noreply.github.com>
2020-06-11 22:54:04 +0700

    Update program.cs

--- program.cs
+++ program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //using Abstraction.AbstractionClass;      //Digunakan apabila menggunakan Abstraction Class
-using Abstraction.Interface;            //Digunakan apabila menggunakan Interface
+using Abstraction.Interface;            //Digunakan apabila menggunakan interface
 
 namespace Abstraction
 {
