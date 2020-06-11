6dc7c6f391c3684b2dabaf3d5bce520b0b29cb61
Lucky728 <61776822+Lucky728@users.noreply.github.com>
2020-06-11 20:46:03 +0700

    Update bangundatar.cs

--- abstractionclass/bangundatar.cs
+++ abstractionclass/bangundatar.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Abstraction.AbstractionClass
+namespace abstraction.AbstractionClass
 {
     public Abstraction class BangunDatar
     {
